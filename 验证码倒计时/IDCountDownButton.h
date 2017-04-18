220b622cee6326d92fe3c9e1fe04ab18a05ad5f4
ZYBest <hzhengkang@163.com>
2017-04-18 14:35:39 +0800

    firstCommit

---
+++ 验证码倒计时/IDCountDownButton.h
@@ -0,0 +1,13 @@
+//
+//  IDCountDownButton.h
+//  验证码倒计时
+//
+//  Created by 胡正康 on 2017/4/6.
+//  Copyright © 2017年 胡正康. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDCountDownButton : UIButton
+
+@end
---
+++ 验证码倒计时/IDCountDownButton.m
@@ -0,0 +1,21 @@
+//
+//  IDCountDownButton.m
+//  验证码倒计时
+//
+//  Created by 胡正康 on 2017/4/6.
+//  Copyright © 2017年 胡正康. All rights reserved.
+//
+
+#import "IDCountDownButton.h"
+
+@implementation IDCountDownButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
