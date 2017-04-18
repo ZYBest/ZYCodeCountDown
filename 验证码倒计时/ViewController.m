03590f947c6a4b3748f51b6697cf4da2b7fe0c59
ZYBest <hzhengkang@163.com>
2017-04-18 14:37:47 +0800

    firstCommit

--- 验证码倒计时/ViewController.m
+++ 验证码倒计时/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
-
+#import "IDCountDownButton.h"
 @interface ViewController ()
-
+/** 验证码倒计时的button */
+@property (nonatomic, strong) IDCountDownButton *vertificationCodeIDCountDownButton;
 @end
 
 @implementation ViewController
@@ -17,13 +18,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self creatMainBtn];
+}
+#pragma mark --- 初始化button
+-(void)creatMainBtn{
+    // 创建vertificationCodeIDCountDownButton
+    self.vertificationCodeIDCountDownButton = [[IDCountDownButton alloc] initWithFrame:CGRectMake(160, 204, 120, 44)];
+    // 添加点击事件
+    [self.vertificationCodeIDCountDownButton addTarget:self action:@selector(vertificationCodeIDCountDownButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    // 设置标题相关属性
+    [self.vertificationCodeIDCountDownButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.vertificationCodeIDCountDownButton setTitle:@"获取验证码" forState:UIControlStateNormal];
+    // 设置背景色
+    [self.vertificationCodeIDCountDownButton setBackgroundColor:[UIColor redColor]];
+    // 设置倒计时时长
+    self.vertificationCodeIDCountDownButton.durationOfCountDown = 60;
+    [self.vertificationCodeIDCountDownButton.titleLabel setFont:[UIFont systemFontOfSize:14]];
+    // 将vertificationCodeIDCountDownButton添加的控制器的view中
+    [self.view addSubview:self.vertificationCodeIDCountDownButton];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)vertificationCodeIDCountDownButtonClick:(UIButton *)button {
+    // TODO：调用服务器接口，获取验证码
 }
+
 
 
 @end
